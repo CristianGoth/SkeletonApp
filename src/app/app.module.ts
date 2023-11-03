e829f3bff765e320216fde267fa9721c1deaa8db
Cristian <101565396+CristianGoth@users.noreply.github.com>
2023-11-03 16:32:22 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { HttpClientModule } from '@angular/common/http';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -29,3 +32,15 @@ import { HttpClientModule } from '@angular/common/http';
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArw74k66EiHy6RrjvkePfO7R2HwqEObWU",
+  authDomain: "tellevoapp-e531c.firebaseapp.com",
+  projectId: "tellevoapp-e531c",
+  storageBucket: "tellevoapp-e531c.appspot.com",
+  messagingSenderId: "435044766433",
+  appId: "1:435044766433:web:ef06402f74225437a8dda0",
+  measurementId: "G-MHF17DT0HV"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
