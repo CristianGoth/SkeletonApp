0f437a23f2914eb872876984a2b77545e8fc6ca4
JeanRiveraa <127343219+JeanRiveraa@users.noreply.github.com>
2023-11-19 21:56:51 -0300

    Add files via upload

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
 
-/**
- * Se genera las rutas mediante una constante de tipo Route
- * como un arreglo que acepta rutas, en este caso se implementa
- * un enrutamiento simple, donde existe un path raiz y se carga
- * el componente Home
- */
+
 const routes: Routes = [
   {
     path: '', //path
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+Mapboxgl.accessToken = 'pk.eyJ1IjoiMTcwMSIsImEiOiJjbHAzOTh2bzQwd2p4MmpvaW10c2Y1MDBtIn0.R43sxtnDVW9VaZT52JgBLw';
+
 if (environment.production) {
   enableProdMode();
 }
